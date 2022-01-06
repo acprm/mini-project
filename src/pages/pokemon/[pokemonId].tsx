61c2aca441c61fbc386e76ff06f897bc2595d871
andikacp | diko <andika.pramoedya@accelbyte.net>
2022-01-06 12:38:01 +0700

    display id in page

--- src/pages/pokemon/[pokemonId].tsx
+++ src/pages/pokemon/[pokemonId].tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 const PokemonDetails: NextPage = () => {
+    const router = useRouter();
+    const {pokemonId} = router.query
     return(
         <>
             <Head>
                 <title>Pokemon Details</title>
             </Head>
             <div className="container flex text-5xl justify-center items-center mx-auto h-screen">
-                Pokemon Details
+                Pokemon Details {pokemonId}
             </div>
         </>
     )
--- src/pages/type/[typeId].tsx
+++ src/pages/type/[typeId].tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router';
 
 const TypeDetails: NextPage = () => {
+    const router = useRouter();
+    const {typeId} = router.query
     return(
         <>
             <Head>
                 <title>Type Details</title>
             </Head>
             <div className="container flex text-5xl justify-center items-center mx-auto h-screen">
-                Type Details
+                Type Details {typeId}
             </div>
         </>
     )
