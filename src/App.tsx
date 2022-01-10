185e07887411b8d66ef50702c2a51ad51b38378c
andikacp | diko <andika.pramoedya@accelbyte.net>
2022-01-10 11:01:03 +0700

    project init using Vite

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import RouteDisplay from './components/RouteDisplay'
+
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<RouteDisplay path='Home'/>}/>
+        <Route path="/favorites" element={<RouteDisplay path='Favorites'/>}/>
+        <Route path="/ability" element={<RouteDisplay path='Ability Details'/>}/>
+        <Route path="/pokemon" element={<RouteDisplay path='Pokemon Details'/>}/>
+        <Route path="/move" element={<RouteDisplay path='Move Details'/>}/>
+        <Route path="/type" element={<RouteDisplay path='Type Details'/>}/>
+          
+      </Routes>
+    </Router>
+  )
+}
+
+
+// class masih fail to compile
+// class App extends React.Component {
+//   render(){
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<RouteDisplay path='Home'/>}/>
+//         <Route path="/favorites" element={<RouteDisplay path='Favorites'/>}/>
+//         <Route path="/ability" element={<RouteDisplay path='Ability Details'/>}/>
+//         <Route path="/pokemon" element={<RouteDisplay path='Pokemon Details'/>}/>
+//         <Route path="/move" element={<RouteDisplay path='Move Details'/>}/>
+//         <Route path="/type" element={<RouteDisplay path='Type Details'/>}/>
+        
+//       </Routes>
+//     </Router>
+//   }
+  
+// }
+
+export default App;
---
+++ src/components/RouteDisplay.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+type Props = {
+    path: string
+}
+
+export default class RouteDisplay extends React.Component<Props>{
+    render(){
+        return(
+            <div className="container flex border mx-auto text-5xl justify-center items-center h-screen ">
+                {this.props.path}
+            </div>
+        )
+    }
+}
