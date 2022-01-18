7aa7d0645a2e8fc14ddb78f73c8e12abbbfc1f36
teszerrakt <zaky.syihab@gmail.com>
2022-01-18 16:35:08 +0700

    Move MenuContainer to containers folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import RouteDisplay from './components/RouteDisplay'
 import Home from './views/Home'
-import HomeContainer from "./containers/HomeContainer";
 import SearchContainer from "./containers/SearchContainer";
+import MenuContainer from "./containers/MenuContainer";
 
 class App extends React.Component {
-  render(){
-    return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/search" element={<SearchContainer/>}/>
-            <Route path="/favorites" element={<RouteDisplay path='Favorites'/>}/>
-            <Route path="/ability" element={<RouteDisplay path='Ability Details'/>}/>
-            <Route path="/pokemon" element={<RouteDisplay path='Pokemon Details'/>}/>
-            <Route path="/move" element={<RouteDisplay path='Move Details'/>}/>
-            <Route path="/type" element={<RouteDisplay path='Type Details'/>}/>
-
-          </Routes>
-        </Router>
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/menu" element={<MenuContainer/>}/>
+                    <Route path="/search" element={<SearchContainer/>}/>
+                    <Route path="/favorites" element={<RouteDisplay path='Favorites'/>}/>
+                    <Route path="/ability" element={<RouteDisplay path='Ability Details'/>}/>
+                    <Route path="/pokemon" element={<RouteDisplay path='Pokemon Details'/>}/>
+                    <Route path="/move" element={<RouteDisplay path='Move Details'/>}/>
+                    <Route path="/type" element={<RouteDisplay path='Type Details'/>}/>
+                </Routes>
+            </Router>
         )
-  }
+    }
 }
 
 export default App;
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,8 @@
 import React, {ChangeEvent} from "react";
 import SearchBox from './SearchBox'
 import {Link} from "react-router-dom";
-import {MdHome} from "react-icons/md"
+import {MdClose, MdHome} from "react-icons/md"
+import MenuContainer from "../../containers/MenuContainer";
 
 export enum Back {HOME, PREV}
 
@@ -13,39 +14,62 @@ interface HeaderProps {
     searchActive?: boolean
 }
 
-export default class Header extends React.Component<HeaderProps> {
+interface HeaderState {
+    menuOpen: boolean
+}
+
+export default class Header extends React.Component<HeaderProps, HeaderState> {
+    state = {
+        menuOpen: false
+    }
 
     static defaultProps = {
         back: Back.PREV,
         favorite: false
     }
 
+    toggleButton = () => {
+        this.setState({menuOpen: !this.state.menuOpen})
+    }
+
+    renderMenuButton () {
+        if (this.props.favorite) {
+            return <img src="/heart-outline.svg" alt="Favorite"/>
+        }
+        else {
+            return (
+                <button onClick={this.toggleButton}>
+                    {this.state.menuOpen ? <MdClose className="text-main-red text-2xl"/> :  <img src="/menu.svg" alt="Menu"/>}
+                </button>
+            )
+        }
+    }
 
     render(): React.ReactNode {
         return (
-            <div className="flex justify-center items-center gap-4 py-5">
+            <div>
+                <div className="flex justify-center items-center gap-4 py-5">
+                    {(this.props.back === Back.PREV) ? (
+                        // TODO make this work to go back to the previous route instead of home
+                        <Link to="/">
+                            <img src="/arrow-back.svg" alt="Back"/>
+                        </Link>
+                    ) : (
+                        <Link to="/">
+                            <MdHome className="text-main-red text-2xl"/>
+                        </Link>
+                    )
+                    }
 
-                {(this.props.back === Back.PREV) ? (
-                    // TODO make this work to go back to the previous route instead of home
-                    <Link to="/">
-                        <img src="/arrow-back.svg" alt="Back"/>
+                    <Link to="/search">
+                        <SearchBox onChange={this.props.searchHandler} term={this.props.searchTerm}
+                                   active={this.props.searchActive}/>
                     </Link>
-                ) : (
-                    <Link to="/">
-                        <MdHome className="text-main-red text-2xl"/>
-                    </Link>
-                )
-                }
-
-                <Link to="/search">
-                    <SearchBox onChange={this.props.searchHandler} term={this.props.searchTerm}
-                               active={this.props.searchActive}/>
-                </Link>
-                <div className="cursor-pointer">
-                    {this.props.favorite && <img src="/heart-outline.svg" alt="Favorite"/>}
-                    {!this.props.favorite && <img src="/menu.svg" alt="Menu"/>}
+                    <div className="cursor-pointer">
+                        {this.renderMenuButton()}
+                    </div>
                 </div>
-
+                {this.state.menuOpen && <MenuContainer/> }
             </div>
         )
     }
