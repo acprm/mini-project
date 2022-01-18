a497c3ba4f6abc4c293f62363f49ca49f54b80a8
teszerrakt <zaky.syihab@gmail.com>
2022-01-18 11:19:06 +0700

    Make the SearchBox always active (visually), and capitalize the uppercase

--- src/components/common/SearchBox.tsx
+++ src/components/common/SearchBox.tsx
@@ -4,6 +4,8 @@ import {MdSearch} from "react-icons/md"
 interface SearchBoxProps {
     term: string
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
+    inputClass?: string
+    iconClass?: string
 }
 
 
@@ -12,15 +14,15 @@ class SearchBox extends Component<SearchBoxProps> {
 
     render() {
         return (
-            <div className="relative text-dark-gray block">
+            <div className={"relative text-dark-gray block"}>
                 <input
-                    className="peer form-input w-full py-3 px-6 rounded-full bg-light-gray focus:outline-main-red focus:bg-white focus:text-black"
+                    className={`peer form-input w-full py-3 px-6 rounded-full bg-light-gray focus:outline-main-red focus:bg-white focus:text-black ${this.props.inputClass}`}
                     onChange={this.props.onChange}
                     value={this.props.term}
                     placeholder="Search here..."
                 />
                 <MdSearch
-                    className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 right-6 peer-focus-within:text-main-red text-xl"/>
+                    className={`pointer-events-none absolute top-1/2 transform -translate-y-1/2 right-6 peer-focus-within:text-main-red text-xl ${this.props.iconClass}`}/>
             </div>
         )
     }
--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -1,11 +1,12 @@
 import {ChangeEvent, Component} from "react";
-import SearchBox from "../components/common/SearchBox";
-import {KeyWord} from "../KeyWord";
 import {Link} from "react-router-dom";
 
+
+import SearchBox from "../components/common/SearchBox";
+import {KeyWord} from "../KeyWord";
+import {capitalizeSingle} from "../helpers/capitalize"
+
 // TODO 1. Create Close Toggle Button
-// TODO 2. Capitalize the uppercase
-// TODO 3. Make the search-box always active (red border)
 
 class SearchContainer extends Component<any, any> {
     state = {term: ""}
@@ -16,13 +17,14 @@ class SearchContainer extends Component<any, any> {
 
     renderSuggestion() {
         const filtered = KeyWord.filter(({name}) => name.toLowerCase().indexOf(this.state.term.toLowerCase()) > -1).slice(0, 10)
+
         return filtered.map(({name, type, id}) => {
             return (
                 <div key={`${type}-${id}`} className="px-6 hover:bg-light-gray rounded-full">
                     <Link to={`${type}/${id}`}>
-                        <div className="flex justify-between py-4">
+                        <div className="flex justify-between py-3">
                             <span className="font-bold">{name}</span>
-                            <span className="text-dark-gray">{type}</span>
+                            <span className="text-dark-gray">{capitalizeSingle(type)}</span>
                         </div>
                         <hr/>
                     </Link>
@@ -34,7 +36,12 @@ class SearchContainer extends Component<any, any> {
     render() {
         return (
             <div className="container mx-auto mt-10">
-                <SearchBox term={this.state.term} onChange={this.handleChange}/>
+                <SearchBox
+                    term={this.state.term}
+                    onChange={this.handleChange}
+                    inputClass="bg-white border-2 border-main-red"
+                    iconClass="text-main-red"
+                />
                 {this.renderSuggestion()}
             </div>
         )
