4b8c1587c770beebb958b906d2ab71a211e8d40d
teszerrakt <zaky.syihab@gmail.com>
2022-01-23 23:09:53 +0700

    fix: the PokemonDetailContainer.tsx now working properly

--- src/containers/PokemonDetailContainer.tsx
+++ src/containers/PokemonDetailContainer.tsx
@@ -1,46 +1,71 @@
 import React from 'react';
+import {RouteComponentProps, withRouter} from 'react-router'
+import {connect} from "react-redux";
+import {RootState} from "../redux/store";
+
+import {fetchPokemon, PokemonState} from "../redux/reducers/pokemonSlice";
+import {fetchMoves, MovesState} from "../redux/reducers/movesSlice";
+import {AbilitiesState, fetchAbilities} from "../redux/reducers/abilitiesSlice";
+
 import Tab from '../components/common/Tab'
 import TabContainer from '../containers/TabContainer'
 import Stats from '../components/pokemon/Stats';
 import List from '../components/common/List';
-import Comment from '../components/comment/Comment';
+import Comment, {CommentProps} from '../components/comment/Comment';
 import CommentForm from '../components/comment/CommentForm';
-import { abilityDummy, moveDummy, pokemonDummy } from '../helpers/dummy';
 import PokeMoveType from '../components/common/PokeMoveType';
 import {PathParamsType} from '../type'
-import {withRouter, RouteComponentProps} from 'react-router'
 
 type Props = RouteComponentProps<PathParamsType> & {
-    
+    pokemon: PokemonState,
+    moves: MovesState,
+    abilities: AbilitiesState,
+    fetchPokemon: ({id}: { id: number | number[], lastId?: number, limit?: number }) => void
+    fetchMoves: ({id}: { id: number | number[], lastId?: number, limit?: number }) => void
+    fetchAbilities: ({id}: { id: number | number[], lastId?: number, limit?: number }) => void
 }
- 
-export interface PokemonDetailState {
-    activeTab: number
-}
- 
-class PokemonDetailContainer extends React.Component<Props, PokemonDetailState> {
-    state = { activeTab: 1 }
 
-    componentDidMount(){
-        // use this to log param 
-        console.log(this.props.match.params.id);
-        
+export interface PokemonDetailState {
+    activeTab: number,
+    comments: CommentProps[]
+}
+
+class PokemonDetailContainer extends React.Component<Props, PokemonDetailState> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {activeTab: 1, comments: []};
     }
 
-    renderPokemon(){
-        return(
+    componentDidMount() {
+        // fetch pokemon detail
+        (async () => {
+            const pokemonId = +this.props.match.params.id
+            await this.props.fetchPokemon({id: pokemonId})
+            await this.props.fetchMoves({id: this.props.pokemon.list[0].moves})
+            await this.props.fetchAbilities({id: this.props.pokemon.list[0].abilities})
+
+            const comments = await localStorage.getItem(`comment-${pokemonId}`)
+            if (comments !== null) {
+                this.setState({comments: JSON.parse(comments)})
+            }
+        })()
+    }
+
+    renderPokemon = () => {
+        return (
             <div className='p-5 flex flex-col'>
                 <div>
-                    <div className='text-4xl font-semibold'>{pokemonDummy[0].name}</div>
-                    <div className='text-lg text-dark-gray'>Pokemon #{pokemonDummy[0].id}</div>
+                    <div className='text-4xl font-semibold'>{this.props.pokemon.list[0].name}</div>
+                    <div className='text-lg text-dark-gray'>Pokemon #{this.props.pokemon.list[0].id}</div>
                 </div>
                 <div className='flex flex-col items-center'>
-                    <img src={pokemonDummy[0].imgUrl} alt="Sprite" height={250} width={250} />
-                    {<div className={`flex ${pokemonDummy[0].type.length === 1 ? 'justify-center' : 'justify-center'} gap-5 w-1/2`}>
-                        {pokemonDummy[0].type.map((item, idx)=>{
-                            return(
-                                <div key={idx}>
-                                    <PokeMoveType id={item} />
+                    <img src={this.props.pokemon.list[0].sprite} alt="Sprite" height={250} width={250}/>
+                    {<div
+                        className={`flex ${this.props.pokemon.list[0].types.length === 1 ? 'justify-center' : 'justify-center'} gap-5 w-1/2`}>
+                        {this.props.pokemon.list[0].types.map((item) => {
+                            return (
+                                <div key={item}>
+                                    <PokeMoveType id={item}/>
                                 </div>
                             )
                         })}
@@ -50,35 +75,42 @@ class PokemonDetailContainer extends React.Component<Props, PokemonDetailState> 
         )
     }
 
-    handleOnTabClick(e:number){
-        this.setState({activeTab:e})
+    handleOnTabClick = (e: number) => {
+        this.setState({activeTab: e})
     }
 
-    render() { 
-        return ( 
+    commentStateRefresh = (newComments: CommentProps[]) => {
+        this.setState({comments: newComments})
+    }
+
+    render() {
+        console.log(this.state.comments)
+        if (this.props.pokemon.list[0]) return (
             <div className='flex flex-col gap-5'>
-                <this.renderPokemon/>
+                {this.renderPokemon()}
 
                 <TabContainer large={false}>
                     <div className='flex justify-center content-center items-center gap-8'>
-                        <Tab name='Stats' active={this.state.activeTab === 1 ? true : false} onClick={() => this.handleOnTabClick(1)}/>
-                        <Tab name='Moves' active={this.state.activeTab === 2 ? true : false} onClick={() => this.handleOnTabClick(2)}/>
-                        <Tab name='Abilities' active={this.state.activeTab === 3 ? true : false} onClick={() => this.handleOnTabClick(3)}/>
-                        <Tab name='Comments' active={this.state.activeTab === 4 ? true : false} onClick={() => this.handleOnTabClick(4)}/>
+                        <Tab name='Stats' active={this.state.activeTab === 1} onClick={() => this.handleOnTabClick(1)}/>
+                        <Tab name='Moves' active={this.state.activeTab === 2} onClick={() => this.handleOnTabClick(2)}/>
+                        <Tab name='Abilities' active={this.state.activeTab === 3}
+                             onClick={() => this.handleOnTabClick(3)}/>
+                        <Tab name='Comments' active={this.state.activeTab === 4}
+                             onClick={() => this.handleOnTabClick(4)}/>
                     </div>
 
                     {/* Stats */}
                     <div className={this.state.activeTab !== 1 ? 'hidden' : 'mt-16 inline-block'}>
-                        <Stats/>
+                        <Stats statList={this.props.pokemon.list[0].stats}/>
                     </div>
 
                     {/* Moves */}
                     <div className={`${this.state.activeTab !== 2 ? 'hidden' : 'inline-block'} overflow-y-auto `}>
-                        {moveDummy && moveDummy.map((item,idx)=>
+                        {this.props.moves.list && this.props.moves.list.map((item, idx) =>
                             (
                                 <div key={idx}>
-                                    <hr className='my-4' />
-                                    <List item={item.name} types={item.types} id={item.id} category='move'/>
+                                    <hr className='my-4'/>
+                                    <List item={item.name} types={[item.type]} id={item.id} category='move'/>
                                 </div>
                             )
                         )}
@@ -86,11 +118,11 @@ class PokemonDetailContainer extends React.Component<Props, PokemonDetailState> 
 
                     {/* Abilities */}
                     <div className={`${this.state.activeTab !== 3 ? 'hidden' : 'inline-block'} overflow-y-auto `}>
-                        {abilityDummy && abilityDummy.map((item,idx)=>
+                        {this.props.abilities.list && this.props.abilities.list.map((item) =>
                             (
-                                <div key={idx}>
-                                    <hr className='my-4' />
-                                    <List item={item.name} id={item.id} category='ability' />
+                                <div key={item.id}>
+                                    <hr className='my-4'/>
+                                    <List item={item.name} id={item.id} category='ability'/>
                                 </div>
                             )
                         )}
@@ -98,18 +130,23 @@ class PokemonDetailContainer extends React.Component<Props, PokemonDetailState> 
 
                     {/* Comments */}
                     <div className={`${this.state.activeTab !== 4 ? 'hidden' : 'inline-block'} overflow-y-auto `}>
-                        <Comment name='User 1' comment='Cool Pokemon' timestamp={new Date} />
-                        <Comment name='User 2' comment='Beats elite 4 in one move' timestamp={new Date} />
-                        <Comment name='User 3' comment='Chose him as my starter, no regrets' timestamp={new Date} />
-                        <Comment name='User 4' comment='Cool' timestamp={new Date} />
-                        <Comment name='User 5' comment='mantap' timestamp={new Date} />
-                        <CommentForm />
+                        <CommentForm pokemonId={+this.props.match.params.id} refreshComment={this.commentStateRefresh}/>
+                        {this.state.comments.map(({comment, name, timestamp}) => <Comment name={name} comment={comment}
+                                                                                          timestamp={timestamp}/>
+                        )}
                     </div>
 
                 </TabContainer>
             </div>
-         );
+        )
+        else return null
     }
 }
- 
-export default withRouter(PokemonDetailContainer);
+
+const mapStateToProps = (state: RootState) => ({
+    pokemon: state.pokemon,
+    moves: state.moves,
+    abilities: state.abilities,
+})
+
+export default connect(mapStateToProps, {fetchPokemon, fetchMoves, fetchAbilities})(withRouter(PokemonDetailContainer));
