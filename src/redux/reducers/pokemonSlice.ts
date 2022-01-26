69540bcca3535162af2da3016a6dbf0596ff63be
teszerrakt <zaky.syihab@gmail.com>
2022-01-26 14:40:34 +0700

    add changeGeneration action

--- src/redux/reducers/pokemonSlice.ts
+++ src/redux/reducers/pokemonSlice.ts
@@ -1,5 +1,5 @@
 import {DataMin, Status} from "../../type";
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {getData} from "../api/getData";
 
 // Minimum Data for Pokemon
@@ -20,13 +20,29 @@ export type PokemonStat = {
     maxStat: number,
 }
 
+export type PokemonGen = "I" | "II" | "III" | "IV" | "V" | "VI" | "VII" | "VIII"
+
+export interface Page {
+    generation: PokemonGen
+    firstId: number,
+    lastId: number,
+    limit: number
+}
+
 export interface PokemonState {
     list: PokemonDetail[]
+    page: Page
     status: Status
 }
 
 const initialState: PokemonState = {
     list: [],
+    page: {
+        generation: "I",
+        firstId: 1,
+        lastId: 151,
+        limit: 24
+    },
     status: "idle"
 }
 
@@ -38,11 +54,24 @@ export const fetchPokemon = createAsyncThunk(
     }
 )
 
+export const fetchMorePokemon = createAsyncThunk(
+    'pokemon/fetchMorePokemon',
+    async ({id, lastId, limit}: { id: number | number[], lastId?: number, limit?: number }) => {
+        const result = await getData("pokemon", id, lastId, limit)
+        return result as unknown as PokemonDetail[]
+    }
+)
 
 export const pokemonSlice = createSlice({
         name: 'pokemon',
         initialState,
-        reducers: {},
+        reducers: {
+            changeGeneration: (state, action: PayloadAction<PokemonGen>) => {
+                // TODO add switch statement based on action.payload
+                // switch (action.payload) {
+                //     case ("IV"): change gen, firstId, lastId into gen IV
+            }
+        },
         extraReducers: builder => {
             builder
                 .addCase(fetchPokemon.pending, (state) => {
@@ -50,13 +79,24 @@ export const pokemonSlice = createSlice({
                 })
                 .addCase(fetchPokemon.fulfilled, (state, {payload}) => {
                     const newState: PokemonState = {
+                        ...state,
                         list: payload,
                         status: "idle"
                     }
-
                     return newState
                 })
                 .addCase(fetchPokemon.rejected, state => {
+                    state.status = "failed"
+                })
+                // Fetch More Pokemon
+                .addCase(fetchMorePokemon.pending, (state) => {
+                    state.status = "loading"
+                })
+                .addCase(fetchMorePokemon.fulfilled, (state, {payload}) => {
+                    state.list.push(...payload)
+                    state.status = "idle"
+                })
+                .addCase(fetchMorePokemon.rejected, state => {
                     state.status = "failed"
                 })
         }
